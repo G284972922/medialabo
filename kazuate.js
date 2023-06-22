3fc8553a75f6323551daff4cf87d49361dc0ad47
G284972022 <228497@st.takushoku-u.ac.jp>
2023-06-22 17:20:00 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -17,8 +17,32 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+    kaisu=kaisu+1;
+    console.log(kaisu+'回目の予想:'+yoso);
+    if(kaisu<3){
+
+        if(yoso===kotae){
+            console.log('正解です。おめでとう!');
+            let seikai=1;
+        }else if(yoso<kotae){
+            console.log('まちがい．答えはもっと大きいですよ');
+        }else if(yoso>kotae){
+            console.log('まちがい．答えはもっと小さいですよ');
+        }
+
+    }else if(kaisu===3){
+
+        if(yoso===kotae){
+            console.log('正解です。おめでとう!');
+            let seikai=1;
+        }else{
+            console.log("まちがい．残念でした答えは"+kotae+"です．");
+        }
+
+    }else if(kaisu>3){
+        console,log("答えは"+kotae+"でした．すでにゲームは終わっています");
+    }
 }
