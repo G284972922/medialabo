f02ddf9be19f5de5f5cde7cb676ca3bcc5edb37a
G284972022 <228497@st.takushoku-u.ac.jp>
2023-06-22 15:57:49 +0900

    a

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -20,6 +20,6 @@ console.log(campus.address);
 for (let n of campus.buildingD) {
     console.log(n);
 }
-for (let i of gakka.name) {
-    console.log(i);
+for (let i of gakka){
+    console.log(i-4);
 }
