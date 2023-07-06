87e6a4c6090c4393716cd20c625dc9b11efeb51f
G284972022 <228497@st.takushoku-u.ac.jp>
2023-07-06 16:38:56 +0900

    s

--- kazuate.js
+++ kazuate.js
@@ -14,7 +14,8 @@ b.addEventListener('click', hantei);
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let y = document.querySelector('input[name="yostxt"]');
-  let yoso = y.value;
+  let ynumber = Number(y.value);
+  let yoso = ynumber
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
@@ -26,31 +27,43 @@ function hantei() {
     
     answer.textContent= yoso;
 
-    let p1 = document.createElement('p'); 
+    let p1 = document.createElement('div'); 
     p1.textContent =kaisu+'回目の予想: '+yoso;
 
-    let Re=document.querySelector('p#result');
+    let Re=document.querySelector('div');
     Re.insertAdjacentElement('beforeend', p1);
 
     if(kaisu<3){
         if(yoso===kotae){
-            console.log('正解です。おめでとう!');
+            let p1 = document.createElement('div'); 
+            p1.textContent ='正解です。おめでとう!';
+            Re.insertAdjacentElement('beforeend', p1);
         }else if(yoso<kotae){
-            console.log('まちがい．答えはもっと大きいですよ');
+            let p1 = document.createElement('div'); 
+            p1.textContent ='まちがい．答えはもっと大きいですよ';
+            Re.insertAdjacentElement('beforeend', p1);
         }else if(yoso>kotae){
-            console.log('まちがい．答えはもっと小さいですよ');
+            let p1 = document.createElement('div'); 
+            p1.textContent ='まちがい．答えはもっと小さいですよ';
+            Re.insertAdjacentElement('beforeend', p1);
         }
 
     }else if(kaisu===3){
 
         if(yoso===kotae){
-            console.log('正解です。おめでとう!');
+            let p1 = document.createElement('div'); 
+            p1.textContent ='正解です。おめでとう!';
+            Re.insertAdjacentElement('beforeend', p1);
             kaisu=kaisu+3;
         }else{
-            console.log("まちがい．残念でした答えは"+kotae+"です．");
+            let p1 = document.createElement('div'); 
+            p1.textContent ='まちがい．残念でした答えは'+kotae+'です．';
+            Re.insertAdjacentElement('beforeend', p1);
         }
 
     }else if(kaisu>3){
-        console.log("答えは"+kotae+"でした．すでにゲームは終わっています");
+        let p1 = document.createElement('div'); 
+        p1.textContent ='答えは'+kotae+'でした．すでにゲームは終わっています';
+        Re.insertAdjacentElement('beforeend', p1);
     }
 }
