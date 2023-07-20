00222a5ee7b188bd368bcacc725bd2c8c9ee10fd
G284972022 <228497@st.takushoku-u.ac.jp>
2023-07-20 17:11:29 +0900

    x

--- weather.js
+++ weather.js
@@ -1,93 +1,87 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-
 ////////// 課題3-2 ここからプログラムを書こう{
-  console.log(data.coord.lon); 
-  console.log(data.coord.lat); 
-  console.log(data.weather[0].description);
-  console.log(data.main.temp_min); 
-  console.log(data.wind.speed);
-  console.log(data.wind.deg);
-  console.log(data.name);
 
-  let Re=document.querySelector('div#result');
-  let p1 = document.createElement('p'); 
-  p1.textContent =data.coord.lon;
-  Re.insertAdjacentElement('beforeend', p1);
-
-  let p2 = document.createElement('p'); 
-  p2.textContent =data.coord.lat;
-  Re.insertAdjacentElement('beforeend', p2);
-
-  let p3 = document.createElement('p'); 
-  p3.textContent =data.weather[0].description;
-  Re.insertAdjacentElement('beforeend', p3);
-  
-  let p4 = document.createElement('p'); 
-  p4.textContent =data.main.temp_min;
-  Re.insertAdjacentElement('beforeend', p4);
-  
-  let p5 = document.createElement('p'); 
-  p5.textContent =data.wind.speed;
-  Re.insertAdjacentElement('beforeend', p5);
-
-  let p6 = document.createElement('p'); 
-  p6.textContent =data.wind.deg;
-  Re.insertAdjacentElement('beforeend', p6);
-
-  let p7 = document.createElement('p'); 
-  p7.textContent =data.name;
-  Re.insertAdjacentElement('beforeend', p7);
-
-  let a = document.querySelector('#serchBotton');
+let a = document.querySelector('#serchBotton');
   a.addEventListener('click',serch );
 
   function serch() {
-    let x = document.querySelector('input[name="KensakuWord"]');
-    let y =x.value;
-    let z = document.querySelector(y);
+
+    let r = document.querySelectorAll('input[name="where"]');
+
+    let SerchId;
+
+    for (let i = 0; i < r.length; i++) {
+      if (r[i].checked) {
+        SerchId = r[i].id;
+        let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + SerchId + '.json';
+        console.log("選択されているラジオボックスのID:", SerchId);
+        console.log("URL:", url);
+        break;
+          }
+        }
+
+         let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+SerchId+'.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
   }
+let  data;
+  // 通信が成功した時の処理
+function showResult(resp) {
+  // サーバから送られてきたデータを出力
+  data = resp.data;
+  
+  let Re=document.querySelector('div#result');
+  Re.innerHTML = ''; // 既存の検索結果をクリア
+
+  let p1 = document.createElement('p'); 
+  p1.textContent ="緯度 :"+data.coord.lon;
+  Re.insertAdjacentElement('beforeend', p1);
+
+  let p2 = document.createElement('p'); 
+  p2.textContent ="経度 :"+data.coord.lat;
+  Re.insertAdjacentElement('beforeend', p2);
+
+  let p3 = document.createElement('p'); 
+  p3.textContent ="天気 :"+data.weather[0].description;
+  Re.insertAdjacentElement('beforeend', p3);
+  
+  let p4 = document.createElement('p'); 
+  p4.textContent ="最低気温 :"+data.main.temp_min;
+  Re.insertAdjacentElement('beforeend', p4);
+  
+  let p5 = document.createElement('p'); 
+  p5.textContent ="風速 :"+data.wind.speed;
+  Re.insertAdjacentElement('beforeend', p5);
+
+  let p6 = document.createElement('p'); 
+  p6.textContent ="風向き :"+data.wind.deg;
+  Re.insertAdjacentElement('beforeend', p6);
+
+  let p7 = document.createElement('p'); 
+  p7.textContent ="都市名 :"+data.name;
+  Re.insertAdjacentElement('beforeend', p7);
+
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
+  }
+
+  // data をコンソールに出力
+  console.log(data);
+
+  // data.x を出力
+  console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+  console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
